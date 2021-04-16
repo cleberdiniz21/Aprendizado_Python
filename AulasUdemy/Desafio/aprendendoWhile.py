d61f003e7cefdd22ea01dded1f5c1fcaa5be1dcf
José Cleber Souza Diniz <81709992+cleberdiniz21@users.noreply.github.com>
2021-04-15 22:19:36 -0300

    Add files via upload

---
+++ AulasUdemy/Desafio/aprendendoWhile.py
@@ -0,0 +1,26 @@
+x = 0
+
+
+while x < 10:
+    y = 0
+
+    while y < 5:
+        print(f'({x},{y})')
+        y += 1
+    x += 1  # x = x + 1
+
+print('FIM')
+
+
+
+# while x < 10:
+#     if x == 3:
+#         x = x+1
+#         # continue pula o proximo laço
+#         break
+#     print(x)
+#     x = x + 1
+
+
+
+print('fim')
---
+++ AulasUdemy/Desafio/calculadora.py
@@ -0,0 +1,34 @@
+
+var = int(input('Digite a tabuada a ser calculada : '))
+var_1 = 1
+var_2 = 2
+var_3 = 3
+var_4 = 4
+var_5 = 5
+var_6 = 6
+var_7 = 7
+var_8 = 8
+var_9 = 9
+var_10 = 10
+
+resultado = var * var_1
+resultado1 = var * var_2
+resultado2 = var * var_3
+resultado3 = var * var_4
+resultado4 = var * var_5
+resultado5 = var * var_6
+resultado6 = var * var_7
+resultado7 = var * var_8
+resultado8 = var * var_9
+resultado9 = var * var_10
+
+print(f'{var} x {var_1} =',  resultado)
+print(f'{var} x {var_2} =',  resultado1)
+print(f'{var} x {var_3} =',  resultado2)
+print(f'{var} x {var_4} =',  resultado3)
+print(f'{var} x {var_5} =',  resultado4)
+print(f'{var} x {var_6} =',  resultado5)
+print(f'{var} x {var_7} =',  resultado6)
+print(f'{var} x {var_8} =',  resultado7)
+print(f'{var} x {var_9:} =',  resultado8)
+print(f'{var} x {var_10} =',  resultado9)
